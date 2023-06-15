94fe8de729c17ea276823f27eb67359ebfa4c0e7
Zachary Atkins <zatkins2@gmail.com>
2023-06-14 23:24:39 -0400

    update setup, update mask names for cc and fejer1

--- setup.py
+++ setup.py
@@ -1,2 +1,11 @@
-from distutils.core import setup
-setup(name='sofind', packages=['sofind'], version='0.0.1')
+from setuptools import setup
+
+setup(
+    name='sofind',
+    packages=['sofind'],
+    version='0.0.1',
+    install_requires=[
+        'pixell>=0.12.0',
+        # 'mnms==0.1.0'
+    ]
+    )
