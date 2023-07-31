4cfa54023ef4866f980babb02d8f27afc5e91faf
Zachary Atkins <zatkins2@gmail.com>
2023-07-31 15:56:19 -0400

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='sofind',
     packages=['sofind'],
-    version='0.0.2',
+    version='0.0.3',
     install_requires=[
         'pixell>=0.12.0',
         # 'mnms==0.0.2'
