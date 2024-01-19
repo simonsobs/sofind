fbaaf65eb9296146ebc25f3190e9165a59e2264e
Irene Abril-Cabezas <79768254+iabrilcabezas@users.noreply.github.com>
2024-01-19 18:03:36 +0000

    fixed bug and separated snfit. init is clean now.

--- sofind/products/beams/__init__.py
+++ sofind/products/beams/__init__.py
@@ -14,8 +14,7 @@ class Beam(Product):
         self.check_product_config_internal_consistency(__name__)
 
     @implements(Product.get_fn)
-    def get_beam_fn(self,  qid, beam_name = None, split_num=0, coadd=False,
-                    tpol = 'T', subproduct='default', basename=False, **kwargs):
+    def get_beam_fn(self, qid, split_num=0, coadd=False, subproduct='default', basename=False, **kwargs):
     
         """Get the full path to a beam product.
 
@@ -23,16 +22,11 @@ class Beam(Product):
         ----------
         qid : str
             Dataset identification string.
-        beam_name: str
-            Name of beam
-            Defaults None sets beam_name = subproduct
         split_num : int, optional
             Split index of the map product, by default 0.
         coadd : bool, optional
             If True, load the corresponding product for the on-disk coadd map,
             by default False. If True, split_num is neglected.
-        tpol: 'T' or 'POL'
-            read temperature beam (includes transfer function) or polarization (without). default T.
         subproduct : str, optional
             Name of mask subproduct to load raw products from, by default 
             'default'.
@@ -47,10 +41,8 @@ class Beam(Product):
             If basename, basename of requested product. Else, full path to
             beam product.
         """
-        if beam_name is None:
-            beam_name = subproduct
-
-        subprod_dict = self.get_subproduct_dict(__name__, subproduct)[beam_name]
+        
+        subprod_dict = self.get_subproduct_dict(__name__, subproduct)
 
         # get the appropriate filename template
         if coadd:
@@ -58,12 +50,10 @@ class Beam(Product):
         else:
             fn_template = subprod_dict['split_beam_file_template']
 
-        TPOL = '' if tpol == 'T' else 'tailonly_'
-
         # get info about the requested array and add kwargs passed to this
         # method call. use this info to format the file template
         fn_kwargs = self.get_qid_kwargs_by_subproduct(__name__, subproduct, qid)
-        fn_kwargs.update(split_num=split_num, TPOL = TPOL, **kwargs)
+        fn_kwargs.update(split_num=split_num, **kwargs)
         fn = fn_template.format(**fn_kwargs)
 
         if basename:
@@ -73,8 +63,8 @@ class Beam(Product):
             return os.path.join(subprod_path, fn)
 
     @implements(Product.read_product)
-    def read_beam(self, qid, beam_name = None, split_num=0, coadd=False, 
-                  tpol = 'T', subproduct='default', loadtxt_kwargs=None, **kwargs):
+    def read_beam(self, qid, split_num=0, coadd=False, subproduct='default',
+                  loadtxt_kwargs=None, **kwargs):
 
         """Read a map product from disk.
 
@@ -108,8 +98,8 @@ class Beam(Product):
         np.array
             The requested beam = [ells, bells]
         """
-        fn = self.get_beam_fn(qid, beam_name = beam_name, split_num=split_num, coadd=coadd,
-                              tpol = tpol, subproduct=subproduct, basename=False, 
+        fn = self.get_beam_fn(qid, split_num=split_num, coadd=coadd,
+                              subproduct=subproduct, basename=False, 
                               **kwargs)
 
         if loadtxt_kwargs is None:
