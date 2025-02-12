66d5f8f5deb91ba5b7a0a99881c196564c215c83
qujia7 <frankqu7@gmail.com>
2025-02-11 20:01:17 -0500

    update cluster model name/ make readme more consistent/ convert downgrade to integer

--- sofind/products/cluster_model/README.md
+++ sofind/products/cluster_model/README.md
@@ -4,23 +4,24 @@
 ## Allowed Models
 | `subproduct` |  `model_type` |
 | ------------ | ------------- |
-| cluster_model.yaml | `dr6v4` |
+| dr6_nemo_cluster_model_snr5.yaml | `dr6v4` |
 
 ## Required Additional Keyword Arguments for `cluster_model`
 | `model_type` | Additional Keywords | Possible Values |
 | ------------ | ------------------- | --------------- |
-| dr6v4        | `frequency`         | `90`, `150`|
+| dr6v4        | `frequency`         | `090`, `150`|
 |              | `SNR`               | Integer values (e.g., `5`, `10`) |
-|              | `downgrade`         | `down1`, `down2`, `down4` |
+|              | `downgrade`         | Integer values (default `2`) |
 
 ## Code Snippets
 
-### Get Cluster Model Filename:
-```python
-get_cluster_model_fn(subproduct='default', frequency='150', SNR=5, downgrade='down2')
+Create instance of the datamodel
+```
+from sofind import DataModel 
+dm = DataModel.from_config('act_dr6v4')
 ```
 
-### Read Cluster Model:
-```python
-read_cluster_model(subproduct='default', frequency='150', SNR=5, downgrade='down2')
+Read Cluster Model:
+```
+dm.read_cluster_model(subproduct='default', frequency = '090',downgrade=2)
 ```
--- sofind/products/cluster_model/__init__.py
+++ sofind/products/cluster_model/__init__.py
@@ -15,68 +15,63 @@ class ClusterModel(Product):
         self.check_product_config_internal_consistency(__name__)
     #################################
 
-    # Feel free to add stuff here
-
     @implements(Product.get_fn)
-    def get_cluster_model_fn(self, frequency='150', SNR=5, downgrade="down2", subproduct='default',
+    def get_cluster_model_fn(self, frequency='150', SNR=5, downgrade=2, subproduct='default',
                              basename=False, **kwargs):
         """
-        Generate the filename for the cluster model file based on the qid and other parameters.
+        Construct the file path for the cluster model based on the provided parameters.
 
-        Parameters:
-        - frequency (str): Frequency of observation (default: '150').
-        - SNR (int): Signal-to-noise ratio threshold (default: 5).
-        - downgrade (str): Downsampling level (default: 'down2').
-        - subproduct (str): Subproduct identifier (default: 'default').
-        - basename (bool): Whether to return only the filename (default: False).
-        - kwargs: Additional keyword arguments.
+        Args:
+            frequency (str, optional): Frequency of observation (default '150').
+            SNR (int, optional): Signal-to-noise ratio threshold (default 5).
+            downgrade (int, optional): Downsampling level as an integer. This will be converted internally
+                to a string in the format 'down{integer}' (default 2).
+            subproduct (str, optional): Identifier for the subproduct (default 'default').
+            basename (bool, optional): If True, returns only the filename instead of the full path (default False).
+            **kwargs: Additional keyword arguments.
 
         Returns:
-        - str: Full path to the cluster model file, or just the filename if basename=True.
+            str: The full file path to the cluster model, or just the filename if basename is True.
         """
+        if not isinstance(downgrade, int):
+            raise TypeError("downgrade must be an integer")
+        downgrade_str = f"down{downgrade}"
+
         # Access the subproduct dictionary and retrieve the filename template
         subprod_dict = self.get_subproduct_dict(__name__, subproduct)
-
-        # Retrieve the appropriate filename template for the cluster model
         fn_template = subprod_dict['cluster_fn_template']
 
-        fn_kwargs = dict(frequency=frequency, SNR=SNR, downgrade=downgrade, **kwargs)
-
-
-        # Format the filename template with the parameters
+        fn_kwargs = dict(frequency=frequency, SNR=SNR, downgrade=downgrade_str, **kwargs)
         fn = fn_template.format(**fn_kwargs)
 
         if basename:
             return fn
         else:
-            # Retrieve the system path to the directory holding the (sub)product files
             subprod_path = self.get_subproduct_path(__name__, subproduct)
             return os.path.join(subprod_path, fn)
 
     @implements(Product.read_product)
-    def read_cluster_model(self, frequency='150', SNR=5, downgrade="down2", subproduct='default', 
+    def read_cluster_model(self, frequency='150', SNR=5, downgrade=2, subproduct='default', 
                            loadtxt_kwargs=None, **kwargs):
         """
-        Read the cluster model data from the file.
+        Read and load the cluster model data from file.
 
-        Parameters:
-        - frequency (str): Frequency of observation (default: '150').
-        - SNR (int): Signal-to-noise ratio threshold (default: 5).
-        - downgrade (str): Downsampling level (default: 'down2').
-        - subproduct (str): Subproduct identifier (default: 'default').
-        - loadtxt_kwargs (dict): Additional arguments for `np.loadtxt`.
-        - kwargs: Additional keyword arguments.
+        Args:
+            frequency (str, optional): Frequency of observation (default '150').
+            SNR (int, optional): Signal-to-noise ratio threshold (default 5).
+            downgrade (int, optional): Downsampling level as an integer. This will be converted internally
+                to a string in the format 'down{integer}' (default 2).
+            subproduct (str, optional): Identifier for the subproduct (default 'default').
+            loadtxt_kwargs (dict, optional): Additional keyword arguments for np.loadtxt (default None).
+            **kwargs: Additional keyword arguments.
 
         Returns:
-        - np.ndarray: Loaded cluster model data.
+            np.ndarray: The loaded cluster model data.
         """
-        # Generate the filename using the provided parameters
         fn = self.get_cluster_model_fn(frequency=frequency, SNR=SNR, downgrade=downgrade, 
                                        subproduct=subproduct, basename=False, **kwargs)
 
-        # Default arguments for `np.loadtxt` if none are provided
         if loadtxt_kwargs is None:
             loadtxt_kwargs = {}
 
-        # Read and return the data
         return enmap.read_map(fn, **loadtxt_kwargs)
