1c3c92f5f96a21ce34ff0c969627aeaa16909d84
Zachary Atkins <zatkins2@gmail.com>
2025-02-17 15:17:42 -0500

    setup now also support non-editable install

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sofind',
-    packages=['sofind'],
-    version='0.0.8',
+    packages=find_packages(),
+    package_data={'sofind': ['datamodels/*.yaml', 'products/*/*.yaml', 'qids/*.yaml']},
+    version='0.0.9',
     install_requires=[
         'pixell>=0.12.0'
         ]
