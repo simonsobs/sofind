0de30b47fc846566608a867235155fafd2192d33
Zachary Atkins <zatkins2@gmail.com>
2025-04-11 15:31:47 -0400

    rename calibrationspkl -> calibrations

--- sofind/products/__init__.py
+++ sofind/products/__init__.py
@@ -7,5 +7,5 @@ from .hotdogs import HotDog
 from .noise_models import NoiseModel
 from .catalogs import Catalog
 from .masks import Mask
-from .calibrationspkl import Calibration
+from .calibrations import Calibration
 from .transfer_func import TransferFunc
