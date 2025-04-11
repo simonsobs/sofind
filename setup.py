c166e7abcbd577fc3b297b54f1ea2b545e33a5ed
Zachary Atkins <zatkins2@gmail.com>
2025-04-11 17:45:31 -0400

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='sofind',
     packages=find_packages(),
     package_data={'sofind': ['datamodels/*.yaml', 'products/*/*.yaml', 'qids/*.yaml']},
-    version='0.0.10',
+    version='0.0.11',
     install_requires=[
         'pixell>=0.12.0'
         ]
